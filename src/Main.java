08511524764749dc5bad16fae3f80c3d8a400f56
Paul Filatow <paulfltv@gmail.com>
2020-12-08 01:30:42 +0300

    Тестирование базового java-приложения проведено. Фактический результат соответствует ожидаемому результату

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,8 @@
 public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int transfer = 500_000_000;
+        int rest = balance - transfer;
+        System.out.println(rest);
+    }
 }
